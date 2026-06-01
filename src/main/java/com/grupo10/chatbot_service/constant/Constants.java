d7e6d681ab2d886c1f6e125e6721833acc4f55ac
BriamCarrasco <briamcarrasco.f@gmail.com>
2026-06-01 19:25:02 -0400

    refactor: crear clase de constantes y eliminar literales duplicados en GroqService

---
+++ src/main/java/com/grupo10/chatbot_service/constant/Constants.java
@@ -0,0 +1,10 @@
+package com.grupo10.chatbot_service.constant;
+
+public final class Constants {
+
+    private Constants() {
+        throw new UnsupportedOperationException("Esta es una clase de utilidades y no puede ser instanciada");
+    }
+
+    public static final String CONTENT_KEY = "content";
+}
