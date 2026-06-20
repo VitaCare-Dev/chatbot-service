dfa3e1ebd5282769540a61638705eaa81252a510
BriamCarrasco <briamcarrasco.f@gmail.com>
2026-06-19 22:58:58 -0400

    refactor: se elimina anotacion deprecada

--- src/main/java/com/grupo10/chatbot_service/repository/ConversacionChatBotRepository.java
+++ src/main/java/com/grupo10/chatbot_service/repository/ConversacionChatBotRepository.java
@@ -1,6 +1,5 @@
 package com.grupo10.chatbot_service.repository;
 
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.grupo10.chatbot_service.model.ConversacionChatBot;
 
